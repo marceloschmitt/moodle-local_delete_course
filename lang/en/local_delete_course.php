8d5f9a3bb9fb450d180b1c6ce947f1babacc05a0
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-11-07 16:13:01 -0300

    Update local_delete_course.php

--- lang/en/local_delete_course.php
+++ lang/en/local_delete_course.php
@@ -24,7 +24,7 @@
 
 $string['alert'] = 'Attention! If you continue this operation the course you are in will be deleted. Think about downloading a backup.';
 $string['delete_course'] = 'Delete course';
-$string['pluginname'] = 'Delete course';
 $string['delete_course:manage'] = 'Delete own courses';
+$string['pluginname'] = 'Delete course';
 $string['deletedcourse'] = 'Deleted course:';
 $string['deletingcourse'] = 'Deleting course';
