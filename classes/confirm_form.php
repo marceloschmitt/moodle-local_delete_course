99f35427dd2a697f249f6012fec2ec6dc72b0a8a
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-11-07 15:57:41 -0300

    Update confirm_form.php

--- classes/confirm_form.php
+++ classes/confirm_form.php
@@ -22,7 +22,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class local_delete_course_confirm_form extends moodleform {
-    
+
     /**
      * This method just overrides the default in order to create the
      * alert message before deleting the course.
