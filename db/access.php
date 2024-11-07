937db7c3ef58224525397dde8469071643b20008
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2024-11-07 16:06:21 -0300

    Update access.php

--- db/access.php
+++ db/access.php
@@ -31,7 +31,7 @@ $capabilities = [
             'contextlevel' => CONTEXT_COURSE,
             'archetypes' => [
                 'editingteacher' => CAP_ALLOW,
-                'manager' => CAP_ALLOW
-        ]
+                'manager' => CAP_ALLOW,
+            ],
     ]
 ];
